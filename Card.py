530e0c2eeadf18c7b32cee7d64c352b011117e38
Nayed <nayed.saidali@gmail.com>
2015-08-27 23:12:09 +0200

    testing

--- Card.py
+++ Card.py
@@ -1,5 +1,5 @@
 class Card:
-    values = (None, None, 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King")
+    values = (None, None, 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "As")
     colors = ("Heart", "Diamonds", "Clubs", "Spades")
 
     def __init__(self, val, col):
---
+++ start.py
@@ -0,0 +1,9 @@
+from Card import Card
+
+if __name__ == '__main__':
+    c = Card(12, 2)
+    c2 = Card(14, 3)
+    print("1rst card: ")
+    print(c)
+    print("\n2nd card: ")
+    c2.display_ascii()
