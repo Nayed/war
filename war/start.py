c967fae4cb646efd4d7464a66ef807d9799054ac
Nayed <nayed.saidali@gmail.com>
2015-08-30 14:49:12 +0200

    test

--- war/start.py
+++ war/start.py
@@ -1,7 +1,9 @@
-from Deck import Deck
+from DeckPlayer import DeckPlayer
+from Card import Card
 
 if __name__ == '__main__':
-    c = Deck()
-    c.shuffle()
-    print("first card is ")
-    print(c.pull())
+    c = Card(12, 2)
+    deck_player = DeckPlayer()
+    deck_player + c 
+    print(deck_player)
+    
