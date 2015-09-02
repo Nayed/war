00d83189c6bdf79567a367dadaa4127a00f644cf
Nayed <nayed.saidali@gmail.com>
2015-09-02 21:05:09 +0200

    classic card

--- war/Card.py
+++ war/Card.py
@@ -1,32 +1,41 @@
 class Card:
-    values = (None, None, 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "As")
-    colors = ("Heart", "Diamonds", "Clubs", "Spades")
 
-    def __getValue(self):
+    values = None
+    colors = None
+
+    def getValue(self):
         return self.__value
 
-    def __setValue(self, val):
+    def setValue(self, val):
         self.__value = val
 
-    value = property(__getValue, __setValue)
+    value = property(getValue, setValue)
+
+    def getColor(self):
+        return self.__color
+
+    def setColor(self, col):
+        self.__color = col
+
+    color = property(getColor, setColor)
 
     def __init__(self, val, col):
-        if val < 2 or val > 14:
-            print("Error: \n\tValue must be between 2 and 14")
-            exit(1)
-        if col < 0 or col > 3:
-            print("Error: \n\tCode color must be between 0 and 3")
-            exit(1)
+        if self.__class__ is Card:
+            raise Exception("Creation forbidden!")
+        self.validation(val, col)
         self.__value = val
         self.__color = col
 
     def __str__(self):
-        return str(Card.values[self.__value]) + " of " + Card.colors[self.__color]
+        return str(Card.values[self.value]) + " of " + Card.colors[self.color]
 
     def display_ascii(self):
-        name = str(Card.values[self.__value]) + " of " + Card.colors[self.__color]
+        name = str(Card.values[self.value]) + " of " + Card.colors[self.color]
         size = len(name) + 2
         print("/", "-" * size, "\\", sep="")
         print("|", name, "|")
         print("|", "-" * size, "|", sep="")
         print("\\", "-" * size, "/", sep="")
+
+    def validation(self, val, col):
+        pass
---
+++ war/ClassicCard.py
@@ -0,0 +1,17 @@
+from Card import Card
+
+class ClassicCard(Card):
+
+    def __init__(self, val, col):
+        Card.values = (None, None, 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "As")
+        Card.colors = ("Heart", "Diamonds", "Clubs", "Spades")
+        super().__init__(val, col)
+
+    def validation(self, val, col):
+        if val < 2 or val > 14:
+            print("Error: \n\tValue must be between 2 and 14")
+            exit(1)
+        if col < 0 or col > 3:
+            print("Error: \n\tCode color must be between 0 and 3")
+            exit(1)
+            
